2d5a4fc5675414dc04e826717fc175383450d999
akshayshelke27490 <akshayshelke27490@gmail.com>
2017-10-16 20:59:01 +0530

    Added the input box and Firebase implementation

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Text,
-  View
-} from 'react-native';
-import { Header } from './components/common/header';
-
-
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import Loginform  from './components/Loginform';
 
 export default class App extends Component {
-  render() {
-    return (
-      <View>
-          <Header headerText="Authentication"/>
-        <Text>
-          Welcome to React Native!
-        </Text>
-      </View>
-    );
-  }
+    componentWillMount(){
+        firebase.initializeApp({
+            apiKey: 'AIzaSyC1o-gp27JI8bX6an45GxRilWVCPMZydgs',
+            authDomain: 'react-native-auth-2adf2.firebaseapp.com',
+            databaseURL: 'https://react-native-auth-2adf2.firebaseio.com',
+            projectId: 'react-native-auth-2adf2',
+            storageBucket: 'react-native-auth-2adf2.appspot.com',
+            messagingSenderId: '337348104508'
+        });
+    }
+    render() {
+        return (
+          <View>
+              <Header headerText="Authentication"/>
+                <Loginform/>
+          </View>
+        );
+    }
 }
